1df914c0c182212654c689cb09f93906977dc368
Makis Arsenis <gerasimos.arsenis@toolsforhumanity.com>
2025-09-08 13:12:36 -0700

    Fix interface

--- src/icf.rs
+++ src/icf.rs
@@ -39,8 +39,6 @@ pub struct IcShare {
 /// An Interval Containment Function.
 #[derive(Clone, Debug)]
 pub struct IntvFn {
-    pub p: IntG,
-    pub q: IntG,
     pub r_in: IntG,
     pub r_out: IntG,
 }
@@ -51,7 +49,8 @@ pub struct Icf<P>
 where
     P: Prg<NUM_BYTES, 2>,
 {
-    f: IntvFn,
+    p: IntG,
+    q: IntG,
     dcf: DcfImpl<NUM_BYTES, NUM_BYTES, P>,
 }
 
@@ -59,17 +58,18 @@ impl<P> Icf<P>
 where
     P: Prg<NUM_BYTES, 2>,
 {
-    pub fn new(f: IntvFn, prg: P) -> Self {
+    pub fn new(p: IntG, q: IntG, prg: P) -> Self {
         Self {
-            f,
+            p,
+            q,
             dcf: DcfImpl::<NUM_BYTES, NUM_BYTES, P>::new(prg),
         }
     }
 
-    pub fn gen(&self, rng: &mut ThreadRng) -> (IcShare, IcShare) {
+    pub fn gen(&self, f: IntvFn, rng: &mut ThreadRng) -> (IcShare, IcShare) {
         let s0s: [[u8; NUM_BYTES]; 2] = rng.random();
 
-        let gamma = self.f.r_in + IntG::max();
+        let gamma = f.r_in + IntG::max();
 
         let cmp_fn = CmpFn {
             alpha: gamma.into(),
@@ -81,20 +81,20 @@ where
 
         let (k0, k1) = split_keys(keys);
 
-        let q_prime = self.f.q + IntG::one();
-        let a_p = self.f.p + self.f.r_in;
-        let a_q = self.f.q + self.f.r_in;
+        let q_prime = self.q + IntG::one();
+        let a_p = self.p + f.r_in;
+        let a_q = self.q + f.r_in;
         let a_q_prime = a_q + IntG::one();
 
         let z0 = IntG::from(rng.random::<IntGPrimitive>());
 
         // TODO: Implement comparison for group elements.
         let ind_1 = IntG::from(a_p > a_q);
-        let ind_2 = IntG::from(a_p > self.f.p);
+        let ind_2 = IntG::from(a_p > self.p);
         let ind_3 = IntG::from(a_q_prime > q_prime);
         let ind_4 = IntG::from(a_q == IntG::max());
 
-        let z1 = z0 + -(self.f.r_out + ind_1 + -ind_2 + ind_3 + ind_4);
+        let z1 = z0 + -(f.r_out + ind_1 + -ind_2 + ind_3 + ind_4);
 
         let k0 = IcShare {
             dcf_share: k0,
@@ -109,11 +109,11 @@ where
     }
 
     pub fn eval(&self, b: bool, k: &IcShare, x: IntG) -> IntG {
-        let q_prime = self.f.q + IntG::one();
+        let q_prime = self.q + IntG::one();
 
         let zero = <IntG as Group<NUM_BYTES>>::zero();
 
-        let x_p = x + IntG::max() + -self.f.p;
+        let x_p = x + IntG::max() + -self.p;
         let mut s_b_p = zero;
         self.dcf
             .eval(b, &k.dcf_share, &[&x_p.into()], &mut [&mut s_b_p]);
@@ -127,7 +127,7 @@ where
             &mut [&mut s_b_q_prime],
         );
 
-        let ind_1 = IntG::from(x > self.f.p);
+        let ind_1 = IntG::from(x > self.p);
         let ind_2 = IntG::from(x > q_prime);
 
         (if b { ind_1 + -ind_2 } else { zero }) + -s_b_p + s_b_q_prime + k.z
